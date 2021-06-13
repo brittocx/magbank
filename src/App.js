ecb9638409ebb28a944ceec7b0a2cf504ccce724
brittocx <brittocx@gmail.com>
2021-06-13 20:19:05 -0300

    Magbank Project Part 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import AccountModal from "./components/AccountModal";
+import "./App.scss";
 
-function App() {
+import Home from "./views/Home";
+import Login from "./views/Login";
+import Dashboard from "./views/Dashboard";
+
+const PrivateRoute = ({ children, logged, ...rest }) => (
+  <Route
+    {...rest}
+    render={() => (logged ? children : <Redirect to="/login" />)}
+  />
+);
+
+const App = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [name, setName] = useState();
+  const [account, setAccount] = useState();
+  const isLogged = name && account;
+
+  const fakeAuth = {
+    login(name, account, cb) {
+      setName(name);
+      setAccount(account);
+      setTimeout(cb, 100);
+    },
+    logout(cb) {
+      setName();
+      setAccount();
+      setTimeout(cb, 100);
+    },
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar
+        handleCreateAcc={() => setShowModal(true)}
+        logged={isLogged}
+        auth={fakeAuth}
+      />
+
+      <Switch>
+        <Route path="/login">
+          <Login auth={fakeAuth} />
+        </Route>
+        <PrivateRoute path="/dashboard" logged={isLogged}>
+          <Dashboard name={name} account={account} />
+        </PrivateRoute>
+        <Route path="/">
+          <Home handleClick={() => setShowModal(true)} />
+        </Route>
+      </Switch>
+
+      <Footer />
+      <AccountModal
+        show={showModal}
+        handleClose={() => setShowModal(false)}
+        auth={fakeAuth}
+      />
+    </Router>
   );
-}
+};
 
 export default App;
